07c0bbb4a89ad165c7dce3a155a14b4f446d131b
JoJo <powerbobo88@gmail.com>
2023-10-23 17:32:58 +0900

    [UPDATE] 23/10/23 PETLOUNGE 시간별기온 시간 수정 및 아이콘 출력 완료
    1. 시간별 기온의 시간이 현재시간으로부터 6시간씩 출력되는 문제가 있어서 기준시간으로부터 6시간씩 출력되게끔 수정함.
    2. 시간별 기온의 시간과 강수량에 맞는 아이콘 출력 완료

--- src/main/java/com/lounge/pet/weather/controller/WeatherController.java
+++ src/main/java/com/lounge/pet/weather/controller/WeatherController.java
@@ -1,20 +1,50 @@
 package com.lounge.pet.weather.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+import com.lounge.pet.user.domain.User;
+import com.lounge.pet.user.service.UserService;
 
 @Controller
 @RequestMapping("/weather")
 public class WeatherController {
+	
+//	@Autowired
+//	private User user;
+	@Autowired
+	private UserService uService;
 
 	// 산책 날씨 예보 페이지로 이동
+	// 회원DB에서 주소 호출
 	@GetMapping("/weather.pet")
-	public String showFaqView() {
-		return "weather/weather";
+	public String showFaqView(User user
+			, HttpSession sessoin
+			, HttpServletRequest request
+			, Model model) {
+		
+		try {
+			HttpSession session = request.getSession(); // HttpSession 객체 가져오기
+			String sessionId = (String)session.getAttribute("uId");
+			if(sessionId != null && sessionId != "") {
+				User uOne = uService.selectOneById(sessionId);
+				model.addAttribute("user", uOne);
+				return "weather/weather";
+			} else {
+				// 기본값 서울
+				return "weather/weather";
+			}
+		} catch (Exception e) {
+			model.addAttribute("msg", "관리자에게 문의바랍니다.");
+			model.addAttribute("url", "/weather/weather.pet");
+			return "common/message";
+		}
 	}
 	
 }
